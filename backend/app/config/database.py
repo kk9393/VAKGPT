145f48eb2bd5437a8420e95b0bd8b0800cf6ca46
ganesh1999 <dganesh1999@gmail.com>
2025-02-07 18:56:09 +0530

    updated mongodb connection string variable name in env

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.config.settings import settings
 
-client = AsyncIOMotorClient(settings.MONGO_URI)
+client = AsyncIOMotorClient(settings.MONGODB_CONNECTION_STRING)
 db = client.auth_db  # Database Name
 users_collection = db.users  # Collection Name
--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    MONGO_URI = os.getenv("MONGO_URI")
+    MONGODB_CONNECTION_STRING = os.getenv("MONGODB_CONNECTION_STRING")
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
     GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
